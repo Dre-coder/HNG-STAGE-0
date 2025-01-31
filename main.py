905880ecad125b30950ac3c02227196af7ca5554
Dre-coder <oshoayomide03@gmail.com>
2025-01-31 13:23:36 +0100

    added to main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, status
 from datetime import datetime, timezone
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 
 
@@ -14,5 +23,5 @@ def user_info():
     return{
         "email": "oshoayomide03@gmail.com",
         "current_datetime": date_time,
-        "github_url":""
+        "github_url":"https://github.com/Dre-coder/HNG-STAGE-0"
     }
